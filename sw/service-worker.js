fe25b6608a7f4712c486f80b5521341f5011ca23
Mpoyo12 <mpoyodigra7@gmail.com>
2025-10-15 22:19:46 +0200

    Fix missing newline at end of service-worker.js

---
+++ sw/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'digra-cache-v1';
+const urlsToCache = [
+  'code.html',
+  'manifest.json',
+  '/', // racine
+];
+
+// Installation
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activation
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+```
